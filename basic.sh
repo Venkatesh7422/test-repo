8f8219ce19c8465797a84b102c08d31629419309
venkatesh <venkateshvenky626@gmail.com>
2025-03-21 16:01:37 +0530

    basics

--- basic.sh
+++ basic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PERSON1=venky
-PERSON2=leela
+PERSON1=$(PERSON1)
+PERSON2=$(PERSON2)
 
-echo "$PERSON1 = hi $PERSON2SURESH"
+echo "$PERSON1 = hi $PERSON2"
 echo "$PERSON2 = hi $PERSON1"
 echo "$PERSON1 = how r u $PERSON2"
 echo "$PERSON1 = i am fine what about you $PERSON2"
